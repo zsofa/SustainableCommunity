d4f0bc9031ba232b6edb456e76962028a42b9dff
Bebyno <bebyno888@gmail.com>
2021-11-15 14:26:09 +0100

    UserController added

---
+++ SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/services/UserService.java
@@ -0,0 +1,13 @@
+package Progmatic.SustainableCommunity.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
