84361bba5363cf7ea3e6c0631fc92474754e8d3c
zsofa <hzsofi12@gmail.com>
2021-11-16 19:52:07 +0100

    edit

--- SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
+++ SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
@@ -1,17 +1,26 @@
 package Progmatic.SustainableCommunity.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUser  { //userdetails
 
     private Long userId;
     private String username; //email
     private String password;
 
-    private LocalDateTime regtime;
+    private LocalDateTime regTime;
     private String firstName;
     private String lastName;
 
     private UserRole userRole;
+
 
 }
