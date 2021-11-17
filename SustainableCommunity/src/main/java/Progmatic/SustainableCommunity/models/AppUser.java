ff24243e3e9491974c22c7ce62c4895f2e78b63c
zsofa <hzsofi12@gmail.com>
2021-11-17 18:06:35 +0100

    getter setter AppUser + implements UserDetailes

--- SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
+++ SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
-/*@Getter @Setter
-@AllArgsConstructor
-@NoArgsConstructor*/
-
+@Getter @Setter
 @Entity
-public class AppUser  { //userdetails
+public class AppUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,6 +34,38 @@ public class AppUser  { //userdetails
     private UserRole userRole;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
+    @Override
+    public String getPassword() {
+        return null;
+    }
 
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
