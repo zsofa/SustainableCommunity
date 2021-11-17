77be0a3fc5420bda314aa02b88abfff26fd17f6e
zsofa <hzsofi12@gmail.com>
2021-11-17 12:12:22 +0100

    add: user DB

--- SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
+++ SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/models/AppUser.java
@@ -4,23 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter @Setter
+/*@Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor*/
+
+@Entity
 public class AppUser  { //userdetails
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Column(length = 100, unique = true)
     private String username; //email
+
     private String password;
 
+    @CreationTimestamp
     private LocalDateTime regTime;
     private String firstName;
     private String lastName;
 
+    @Enumerated
     private UserRole userRole;
+
+
 
 
 }
