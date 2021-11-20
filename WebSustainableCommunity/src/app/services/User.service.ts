e28e45c4a287e2065776f6c6b476ff18f7ef2519
Bebyno <bebyno888@gmail.com>
2021-11-20 13:08:37 +0100

    beauty

--- WebSustainableCommunity/src/app/services/User.service.ts
+++ WebSustainableCommunity/src/app/services/User.service.ts
@@ -21,17 +21,18 @@ public currentUser: User = null;
 
 
   login(name: string, pass: string) {
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].username === name && this.users[i].password === pass) {
-        this.currentUser = this.users[i];
-        console.log(this.currentUser);
-        this.router.navigateByUrl("/productList");
-        return;
-      }
+    // for (let i = 0; i < this.users.length; i++) {
+    //   if (this.users[i].username === name && this.users[i].password === pass) {
+    //     this.currentUser = this.users[i];
+    //     console.log(this.currentUser);
+    //     this.router.navigateByUrl("/home");
+    //     return;
+    //   }
 
-    }
-    alert($localize `Wrong username or password`);
-    return;
+    // }
+    this.currentUser = new User();
+   // alert($localize `Wrong username or password`);
+    //return;
 
   }
 
