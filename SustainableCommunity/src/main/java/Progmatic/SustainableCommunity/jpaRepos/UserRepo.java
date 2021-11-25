d326ce857d13164e91e44b38d800a6660dd68b2b
Alexandra <hajdu.alexandra09@gmail.com>
2021-11-25 15:06:05 +0100

    - update query for set enabled true in UserRepo added

--- SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/jpaRepos/UserRepo.java
+++ SustainableCommunity/src/main/java/Progmatic/SustainableCommunity/jpaRepos/UserRepo.java
@@ -2,11 +2,20 @@ package Progmatic.SustainableCommunity.jpaRepos;
 
 import Progmatic.SustainableCommunity.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepo extends JpaRepository<AppUser, Long> {
 
     AppUser findAppUserByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableAppUser(String email);
 
 }
